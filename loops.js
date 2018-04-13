081cb488973df3e5f7f47df0b25199742be182cb
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-13 16:48:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -41,9 +41,9 @@ function maybeTrue() {
 //   return array
 // }
 
-// function doWhileLoop(array) {
-//   do {
-//     array.pop()
-//   } while (array.length > 0 && maybeTrue());
-//   return array
-// }
+function doWhileLoop(array) {
+  do {
+    array.pop()
+  } while (array.length > 0 && maybeTrue());
+  return array
+}
