bb14af3c6bec41cc6c5d7d7b2845974cbde3b6ed
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-13 16:26:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -9,16 +9,16 @@
 //   return array
 // }
 
-function forLoop(array) {
-  for (var i = 0; i < 25; i++) {
-    array.push(`${i === 1 ? `I am ${i} strange loop` : `I am ${i} strange loops`}`)
-  }
-  return array
-}
-
 // function forLoop(array) {
 //   for (var i = 0; i < 25; i++) {
-//     array.push(`I am ${i} strange loop${i === 1 ? `` : `s`}`)
+//     array.push(`${i === 1 ? `I am ${i} strange loop` : `I am ${i} strange loops`}`)
 //   }
 //   return array
 // }
+
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loop${i === 1 ? `` : `s`}`)
+  }
+  return array
+}
