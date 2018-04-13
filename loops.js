3896795836552b5db93bc9ccb813e71df977ecdc
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-13 16:10:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,9 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
     if (i === 1) {
-      array.push()
+      array.push('I am 1 strange loop')
+    } else {
+      array.push('I am ${i} strange loops')
     }
   }
 }
